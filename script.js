b106b0fc9f76ef7b26b8ac82006cbf647657f803
Blessing Udoh <105555890+myra001@users.noreply.github.com>
2022-06-25 10:09:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstName = "Myra"
+const another_variable ="45"
+
+//another_variable= "something else"
+
+firstName="Blessing"
+let last_name="Udoh"
+
+console.log(`Hello, my name is ${firstName} ${last_name}`)
+console.log(`I am ${5} feet and ${4} inches tall`)
+console.log(`I am from Nigeria`)
